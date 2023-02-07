660a3be738cc3a38154255ddf7a721cd698831df
PNS212624\Hansol <yrlee00@hansol.com>
2023-02-07 17:25:04 +0900

    2/7 commit

---
+++ src/main/java/com/example/springcalculator/component/iCalculator.java
@@ -0,0 +1,9 @@
+package com.example.springcalculator.component;
+
+public interface iCalculator {
+
+    int sum(int x, int y);
+    int minus(int x, int y);
+
+    void init();
+}
---
+++ src/test/java/com/example/springcalculator/component/DollarCalculatorTest.java
@@ -0,0 +1,35 @@
+package com.example.springcalculator.component;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // bean 모두 등록
+class DollarCalculatorTest {
+
+    @MockBean
+    private MarketApi marketApi;
+
+    @Autowired
+    private DollarCalculator dollarCalculator;
+
+    @Test
+    public void dollerCalculatorTest(){
+        Mockito.when(marketApi.connect()).thenReturn(3000);
+        dollarCalculator.init();
+
+        int sum = dollarCalculator.sum(10, 10);
+        int minus = dollarCalculator.minus(10, 10);
+
+        Assertions.assertEquals(60000, sum);
+        Assertions.assertEquals(0, minus);
+
+    }
+}
